a6191719701f45c8f1fdf64ef96d95b343d2d114
Jordhy Branenda <ethgalleryin@gmail.com>
2025-12-29 20:27:56 +0800

    feat: add publications data

--- app/data/publications.ts
+++ app/data/publications.ts
@@ -15,5 +15,13 @@ export const publications: Publication[] = [
     date: "June 2025",
     link: "https://informatika.stei.itb.ac.id/~rinaldi.munir/Matdis/2024-2025-2/Makalah2025/Makalah-Matdis-2025-IF-ITB%20(33).pdf",
     categories: ["Knowledge Graphs", "AI Security", "Discrete Mathematics", "Graph Theory", "Anomaly Detection"],
+  },
+  {
+    id: "2",
+    title: "Efficient Transformer Compression in Pre-trained Language Models Through Selective Tensor Rank Reduction",
+    publisher: "STEI ITB",
+    date: "Dec 2025",
+    link: "https://informatika.stei.itb.ac.id/~rinaldi.munir/AljabarGeometri/2025-2026/Makalah/Makalah-IF2023-Algeo-2025%20(30).pdf",
+    categories: ["Model Compression", "Natural Language Processing", "Transformer Efficiency", "Tensor Decomposition", "Low Rank Approximation", "Geometric", "Linear Algebra"],
   }
 ];

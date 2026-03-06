dad6a8b7e0ff9c8a8adf36cda3868f53f0bf4a5b
Jordhy Branenda <ethgalleryin@gmail.com>
2026-03-07 00:43:32 +0700

    chore: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import StickyBlur from "./components/StickyBlur";
 
 export const metadata: Metadata = {
-  title: "Jordhy Branenda - Portfolio",
+  title: "ethjor",
   description: "Computer Science @ ITB - Learning relentlessly. Building intentionally.",
   icons: {
     icon: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'%3E%3Ctext y='0.9em' font-size='90'%3E👨🏻‍💻%3C/text%3E%3C/svg%3E",
